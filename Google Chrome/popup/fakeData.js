72fabe84e5b5afd376261ed1f551b7a529f41905
viki53 <viki53000@gmail.com>
2014-07-14 23:14:46 +0200

    No-jQuery : popup
    La popup peut déjà se passer de jQuery

---
+++ Google Chrome/popup/fakeData.js
@@ -0,0 +1,14 @@
+var fakeTestData = {
+	notifs: [
+		{
+			id: 1,
+			date: "Il y a 28 minutes",
+			link: "https://zestedesavoir.com/",
+			type: "mp",
+			answerer: {
+				username: "viki53",
+				avatar: "https://zestedesavoir.com/media/galleries/360/951ce9fa-d03f-4107-9c7e-ee8996ff8aa9.png",
+			}
+		}
+	]
+};
--- Google Chrome/popup/popup.js
+++ Google Chrome/popup/popup.js
@@ -3,122 +3,196 @@ var notificator;
 var urlZdS = "http://zestedesavoir.com";
 
 var linkListener = function(notificator, event) {
-    var url = $(event.currentTarget).attr("href");
-    if(url == "#")
-        return;
+	console.dir(this);
+	// return;
 
-    event.preventDefault();
+	var url = this.href;
+	if(url === "#")
+		return;
 
-    var parent = $(event.currentTarget).closest('div'); //trouve l'id du parent
-    var isShortcut = $(parent).hasClass("allNotifs");
-    if(!isShortcut) {
-        var id = $(parent).attr('id').slice(6);
-        var isAlerte = $(parent).hasClass("alerte");
-    }
-    
-    //console.log(url);
-    chrome.windows.getCurrent({ populate: true }, function(currentWindow) {
-        var tab = false;
-        for(var i in currentWindow.tabs) {
-            if(currentWindow.tabs[i].active) {
-                tab = currentWindow.tabs[i];
-                break;
-            }
-        }
-        if(!notificator.getOptions("openInNewTab") && tab && tab.url !== undefined && tab.url.indexOf("zestedesavoir.com") != -1 && tab.url.indexOf("zestedesavoir.com") < 14) {
-            if(isAlerte) {
-                notificator.alertTabId.push(tab.id);
-            }
-            chrome.tabs.update(tab.id, { url: url });
-        }
-        else {
-            chrome.tabs.create({
-                'url': url,
-                'active': false
-            }, function(tab){
-                if(isAlerte) {
-                    notificator.alertTabId.push(tab.id); //on ajout l'id du tab
-                }
-            });
-        }
+	event.preventDefault();
 
-        if(notificator.getOptions("autoclosePopup")) {
-            window.close();
-        }
-    });
+	var isShortcut = this.classList.contains("allNotifs");
+	if(!isShortcut) {
+		var id = this.dataset.notificationId;
+		var isAlerte = this.classList.contains("alerte");
+	}
+	
+	//console.log(url);
+	chrome.windows.getCurrent({ populate: true }, function(currentWindow) {
+		var tab = false;
+		for(var i in currentWindow.tabs) {
+			if(currentWindow.tabs[i].active) {
+				tab = currentWindow.tabs[i];
+				break;
+			}
+		}
+		if(!notificator.getOptions("openInNewTab") && tab && tab.url !== undefined && tab.url.indexOf("zestedesavoir.com") != -1 && tab.url.indexOf("zestedesavoir.com") < 14) {
+			if(isAlerte) {
+				notificator.alertTabId.push(tab.id);
+			}
+			chrome.tabs.update(tab.id, { url: url });
+		}
+		else {
+			chrome.tabs.create({
+				'url': url,
+				'active': false
+			}, function(tab){
+				if(isAlerte) {
+					notificator.alertTabId.push(tab.id); //on ajout l'id du tab
+				}
+			});
+		}
+
+		if(notificator.getOptions("autoclosePopup")) {
+			window.close();
+		}
+	});
 }
 
 var createNotif = function(notif) {
-    var elem = $("<div>", { class: "element", id: "notif-" + notif.id }),
-        notifLink = $("<a>").appendTo(elem);
-    
-    elem.addClass(notif.type);
-    notifLink.attr("href", urlZdS + notif["link"]);
-        
-    $("<img>", { class: "posteur" }).attr("src", notif["answerer"]["avatar"]).appendTo(notifLink);
-    $("<span>", { class: "posteur" }).text(notif["answerer"]["username"]).appendTo(notifLink);
-    $("<span>", { class: "date" }).text(notif["date"]).appendTo(notifLink);
-    $("<span>", { class: "titre" }).text(notif["title"]).appendTo(notifLink);
-    
-    return elem;
+	var notif_link = document.createElement("a");
+	
+	notif_link.href = urlZdS + notif.link;
+
+	notif_link.classList.add("notification");
+	notif_link.classList.add(notif.type);
+
+	notif_link.id = "notif-" + notif.id;
+	notif_link.dataset.notificationId = notif.id;
+
+
+	if(!notif.answerer.avatar.match(/^(http:\/\/|https:\/\/|\/\/)(.*)/i)) {
+		notif.answerer.avatar = urlZdS + notif.answerer.avatar;
+	}
+	var notif_avatar = document.createElement("img");
+	notif_avatar.src = notif.answerer.avatar;
+	notif_avatar.classList.add("user-avatar");
+	notif_avatar.alt = "Avatar de " + notif.answerer.username;
+
+	notif_link.appendChild(notif_avatar);
+
+
+	var notif_pseudo = document.createElement("span");
+	notif_pseudo.classList.add("user-name");
+	notif_pseudo.textContent = notif.answerer.username;
+
+
+	var notif_date = document.createElement("time");
+	notif_date.classList.add("date");
+	notif_date.textContent = notif.date;
+
+	notif_link.appendChild(notif_date);
+
+
+	var notif_title = document.createElement("span");
+	notif_title.classList.add("title");
+	notif_title.textContent = notif.title;
+
+	notif_link.appendChild(notif_title);
+	
+	return notif_link;
 };
 
 var backgroundLoaded = function(bgWindow) {
-    if(!bgWindow || !bgWindow.theNotificator) {
-        console.error("ZdSNotificator : Failed to load background");
-        return;
-    }
-    notificator = bgWindow.theNotificator;
-    var notifs = notificator.getNotification();
-    
-    if(notificator.logged || notificator.useFakeData) {
-        var len = notifs.length;
-        var content = $("#content");
-        
-        var notifList = $("<div>", { class: "notifList" });
-        
-        if(len == 0) {
-            $("<div>", { class: "element other noNotifs" }).append($("<span>").text("Aucune nouvelle notifications")).appendTo(content);
-        } else {
-            for(var i = 0; i < len; i++) {
-                var n = createNotif(notifs[i]).appendTo(notifList);
-            }
-        }
-        
-        notifList.appendTo(content);
-        
-        //ligne "Afficher toute les notifications"
-        if(notificator.getOptions("showAllNotifButton")) {
-            $("<div>", { class: "element other allNotifs" }).append(
-                $("<a>", { href: urlZdS + "/forums/notifications" }).text("Toutes les notifications")
-            ).appendTo(content);
-        }
-        
-        //ligne "Ouvrir le ZdS"
-        if(notificator.getOptions("ZdSLink")) {
-            $("<div>", { class: "element other allNotifs" }).append(
-                $("<a>", { href: urlZdS }).text("Aller sur ZdS")
-            ).appendTo(content);
-        }
-        
-        var liens = document.getElementsByTagName("a");
-        for (var i = 0; i < liens.length; i++) {
-            $(liens[i]).on("click", linkListener.bind(this, notificator));
-        }
-        
-        notificator.setNewNotifCallback(function(notif) {
-            $(".noNotifs").hide();
-            createNotif(notif).appendTo(notifList).find("a").on("click", linkListener.bind(this, notificator));
-        });
-    }
-    else {
-        var content = $("#content");
-        $("<div>", { class: "not-connected"}).html("Vous n'&ecirc;tes pas connect&eacute;!").appendTo(content);
-    }
+	console.dir(bgWindow);
+	if(!bgWindow || !bgWindow.theNotificator) {
+		console.error("ZdSNotificator : Failed to load background");
+		return;
+	}
+	notificator = bgWindow.theNotificator;
+	var notifs = notificator.getNotification();
+
+	// var notifs = fakeTestData.notifs;
+
+	console.dir(notifs);
+
+	var content = document.getElementById("content");
+
+	if(notificator.logged || notificator.useFakeData) {
+		var len = notifs.length;
+		
+		var notifList = document.createElement("div");
+		notifList.classList.add("notifList");
+		
+		if(len === 0) {
+			var no_notifs_elem = document.createElement("div");
+			no_notifs_elem.classList.add("element", "other", "noNotifs");
+
+			var no_notifs_text = document.createElement("span");
+			no_notifs_text.textContent = "Aucune nouvelle notifications";
+
+			no_notifs_elem.appendChild(no_notifs_text);
+			content.appendChild(no_notifs_elem);
+		} else {
+			for(var i = 0; i < len; i++) {
+				var n = createNotif(notifs[i]);
+				notifList.appendChild(n);
+			}
+
+			content.appendChild(notifList);
+		}
+		
+		
+		//ligne "Afficher toute les notifications"
+		if(notificator.getOptions("showAllNotifButton")) {
+			var all_notifs_elem = document.createElement("div");
+			all_notifs_elem.classList.add("element", "other", "allNotifs");
+
+			var all_notifs_link = document.createElement("a");
+			all_notifs_link.href = urlZdS + "/forums/notifications";
+			all_notifs_link.textContent = "Toutes les notifications";
+
+			all_notifs_elem.appendChild(all_notifs_link);
+			content.appendChild(all_notifs_elem);
+		}
+		
+		//ligne "Ouvrir le ZdS"
+		if(notificator.getOptions("ZdSLink")) {
+			var goto_zds_elem = document.createElement("div");
+			goto_zds_elem.classList.add("element", "other", "allNotifs", "goToZdS");
+
+			var goto_zds_link = document.createElement("a");
+			goto_zds_link.href = urlZdS;
+			goto_zds_link.textContent = "Aller sur ZdS";
+			
+			goto_zds_elem.appendChild(goto_zds_link);
+			content.appendChild(goto_zds_elem);
+		}
+		
+		var liens = document.getElementsByTagName("a");
+		for (var i = 0; i < liens.length; i++) {
+			liens[i].addEventListener("click", linkListener.bind(liens[i], notificator), false);
+		}
+		
+		notificator.setNewNotifCallback(function(notif) {
+			var no_notifs_elem = document.getElementsByClassName("noNotifs");
+			if (no_notifs_elem) {
+				no_notifs_elem[0].parentNode.removeChild(no_notifs_elem[0]);
+			}
+
+			var n = createNotif(notif);
+
+			var liens = n.getElementsByTagName("a");
+
+			for (var i = 0; i < liens.length; i++) {
+				liens[i].addEventListener("click", linkListener.bind(liens[i], notificator), false);
+			}
+
+			notifList.appendChild(n);
+		});
+	}
+	else {
+		var not_logged_in_elem = document.createElement("div");
+		not_logged_in_elem.className = "not-connected";
+		not_logged_in_elem.textContent = "Vous n'êtes pas connecté !";
+
+		content.appendChild(not_logged_in_elem);
+	}
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-    chrome.runtime.getBackgroundPage(function() {
-        backgroundLoaded.apply(this, arguments);
-    });
+	chrome.runtime.getBackgroundPage(function() {
+		backgroundLoaded.apply(this, arguments);
+	});
 });
