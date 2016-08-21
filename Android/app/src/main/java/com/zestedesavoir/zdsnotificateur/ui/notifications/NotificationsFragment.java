42fcb989499d9f34d824dab00cbb935319eeaadf
GerardPaligot <gerard.paligot@gmail.com>
2016-08-21 13:40:33 +0200

    feat(loading): Adds a progress bar when load notification.

--- Android/app/src/main/java/com/zestedesavoir/zdsnotificateur/ui/notifications/NotificationsFragment.java
+++ Android/app/src/main/java/com/zestedesavoir/zdsnotificateur/ui/notifications/NotificationsFragment.java
@@ -1,6 +1,10 @@
 package com.zestedesavoir.zdsnotificateur.ui.notifications;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.zestedesavoir.zdsnotificateur.R;
 import com.zestedesavoir.zdsnotificateur.internal.Callback;
@@ -31,6 +36,7 @@ public class NotificationsFragment extends Fragment {
     return new NotificationsFragment();
   }
 
+  @BindView(R.id.progress) ProgressBar pbLoading;
   @BindView(R.id.rv_notifications) RecyclerView rvNotifications;
 
   private OnMainNavigation listener;
@@ -49,6 +55,7 @@ public class NotificationsFragment extends Fragment {
 
   @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    showProgress(true);
 
     final ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
     if (ab != null) {
@@ -63,6 +70,7 @@ public class NotificationsFragment extends Fragment {
     ZdSLibrary.get(getContext()).getNotificationManager().getAll(new Callback<List<Notification>>() {
       @Override public void success(List<Notification> notifications) {
         adapter.filter(Type.NONE, notifications);
+        showProgress(false);
       }
 
       @Override public void failure(Throwable e) {
@@ -109,5 +117,35 @@ public class NotificationsFragment extends Fragment {
   public void onDetach() {
     super.onDetach();
     listener = null;
+  }
+
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2) protected void showProgress(final boolean show) {
+    // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+    // for very easy animations. If available, use these APIs to fade-in
+    // the progress spinner.
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+      int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+      rvNotifications.setVisibility(show ? View.GONE : View.VISIBLE);
+      rvNotifications.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+        @Override
+        public void onAnimationEnd(Animator animation) {
+          rvNotifications.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+      });
+
+      pbLoading.setVisibility(show ? View.VISIBLE : View.GONE);
+      pbLoading.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+        @Override
+        public void onAnimationEnd(Animator animation) {
+          pbLoading.setVisibility(show ? View.VISIBLE : View.GONE);
+        }
+      });
+    } else {
+      // The ViewPropertyAnimator APIs are not available, so simply show
+      // and hide the relevant UI components.
+      pbLoading.setVisibility(show ? View.VISIBLE : View.GONE);
+      rvNotifications.setVisibility(show ? View.GONE : View.VISIBLE);
+    }
   }
 }
