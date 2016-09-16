d3664160cd93a2786855adc28307cbe6ed3db671
AmarOk <blin.sebastien@enconn.fr>
2016-09-16 16:42:01 -0400

    Ajout d'une mise à jour lors de l'ouverture d'une notification

--- WebExtensions/popup/notifier.js
+++ WebExtensions/popup/notifier.js
@@ -1,10 +1,29 @@
-if(chrome.extension.getBackgroundPage()._connected) {
-  var notConnectedDiv = document.getElementById('notConnected');
-  notConnectedDiv.style.display = 'none';
-  var notifications = document.getElementById('notificationList');
-  var bgNodes = chrome.extension.getBackgroundPage()._currentDom.lastChild.cloneNode(true);
-  notifications.appendChild(bgNodes);
-} else {
-  var connectedDiv = document.getElementById('connected');
-  connectedDiv.style.display = 'none';
+var _notifications = document.getElementById('notificationList');
+
+function updateUI() {
+  if(chrome.extension.getBackgroundPage()._connected) {
+    var notConnectedDiv = document.getElementById('notConnected');
+    notConnectedDiv.style.display = 'none';
+    var bgNodes = chrome.extension.getBackgroundPage()._currentDom.lastChild.cloneNode(true);
+    _notifications.appendChild(bgNodes);
+  } else {
+    var connectedDiv = document.getElementById('connected');
+    connectedDiv.style.display = 'none';
+  }
 }
+
+// sleep time expects milliseconds
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+_notifications.addEventListener("click", function(){
+  sleep(2000).then(() => {
+    if(chrome.extension.getBackgroundPage()._notifCounter !== 0) {
+      close();
+    }
+    chrome.extension.getBackgroundPage().getNotificationsFromAPI();
+  });
+});
+
+updateUI();
