2d19c398965ecc378f3f3015d5c0c43f7da4e8bb
AmarOk <contact@enconn.fr>
2017-09-11 22:19:49 -0400

    firefox: Nettoie le code et ajoute un bouton pour créer un nouveau MP

--- Firefox/notifier.js
+++ Firefox/notifier.js
@@ -1,177 +1,200 @@
-/*
-* Global variables
-*/
-var _notifCounter = 0;
-var _delayUpdate = 60 * 1000;
-//Used by the popup
-var _currentDom = null;
-var _contentDiv = null;
-//If the user is connected
-var _connected = false;
-//If we want to see popup variables
-var _showNotification = false;
-chrome.storage.local.get('notify', (res) => {
-  _showNotification = res.notify || false;
-});
-//If we are in debug mode
-var _debug = false;
-var _base_url = "https://zestedesavoir.com/";
-var _token = "zds-notifier-firefox";
-if(_debug) _base_url = "https://beta.zestedesavoir.com/";
+/* global chrome XMLHttpRequest:true */
+/* eslint-disable no-console */
+const debugMode = false
+const NOT_CONNECTED = 401
+const CONNECTED = 200
 
-function escapeHTML(str) { return str.replace(/[&"'<>]/g, (m) => escapeHTML.replacements[m]); }
-escapeHTML.replacements = { "&": "&amp;", '"': "&quot;", "'": "&#39;", "<": "&lt;", ">": "&gt;" };
+// TODO move this
+var connected = false
+var notifCounter = 0
+var currentDom = null
+let contentDiv = null
+
+// Load preferences
+const updateDelay = 60 * 1000
+let showPopupNotification = false
+chrome.storage.local.get('notify', (res) => {
+  showPopupNotification = res.notify || false
+})
+
+let baseUrl = 'https://zestedesavoir.com/'
+if (debugMode) baseUrl = 'https://beta.zestedesavoir.com/'
+const token = 'zds-notifier'
 
 /**
-* getNotificationsFromAPI
-*/
-function getNotificationsFromAPI() {
-  _contentDiv = document.createElement('div');
-  var target = _base_url + "api/notifications/?page_size=30&ordering=-pubdate&Authorization=" + _token;
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", target, true);
+ * Get notifications from https://zestedesavoir.com via the API
+ */
+function getNotificationsFromAPI () {
+  contentDiv = document.createElement('div')
+  const options = `page_size=30&ordering=-pubdate&Authorization=${token}`
+  const target = `${baseUrl}api/notifications/?${options}`
+  // TODO use fetch instead
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', target, true)
   xhr.onload = function (e) {
     if (xhr.readyState === 4) {
-      var result = xhr.status;
-      if(result === 401) {
-        _connected = false;
-        if(_debug) console.log("Not connected");
-        //Change popup image
-        chrome.browserAction.setIcon({path:"icons/notconnected.png"});
-      } else if (result === 200) {
-        _connected = true;
-        var rootDOM = JSON.parse(xhr.response);
-        if(rootDOM.details) {
-          if(_debug) console.log("Error while parsing");
+      const result = xhr.status
+      if (result === NOT_CONNECTED) {
+        connected = false
+        if (debugMode) console.log('Not connected')
+        chrome.browserAction.setIcon({path: 'icons/notconnected.png'})
+      } else if (result === CONNECTED) {
+        connected = true
+        const rootDOM = JSON.parse(xhr.response)
+        if (rootDOM.details) {
+          if (debugMode) console.log('can\'t parse incorrect JSON')
         } else {
-          //Get new notifications
-          var resultsNotification = rootDOM.results;
-          var countNotifications = 0;
-          for(var notif = 0; notif < resultsNotification.length; ++notif) {
-            //If a notification is new we have is_read === False
-            if(!resultsNotification[notif].is_read) {
-              countNotifications += 1;
-              var titleNotif = resultsNotification[notif].title
-              var senderNotif = resultsNotification[notif].sender.username;
-              var senderAvatarNotif = resultsNotification[notif].sender.avatar_url;
-              var dateNotif = resultsNotification[notif].pubdate;
-              var date = new Date((dateNotif || "").replace(/-/g,"/").replace(/[TZ]/g," "));
-              var minutes = '' + date.getMinutes();
-              if(minutes.length < 2) {
-                minutes = '0' + minutes;
-              }
-              var formatedDate = 'le ' + [date.getDate(),
-               date.getMonth()+1].join('/') + ' à ' +
-              [date.getHours(),
-               minutes].join('h');
-              var actualDate = new Date();
-              if(date.getDate() == actualDate.getDate() &&
-                 date.getMonth() == actualDate.getMonth() &&
-                 date.getYear() == actualDate.getYear()) {
-                   formatedDate = "Aujourd'hui";
-              } else {
-                var yesterday = actualDate;
-                yesterday.setDate(actualDate.getDate() - 1);
-                if(date.getDate() == yesterday.getDate() &&
-                   date.getMonth() == yesterday.getMonth() &&
-                   date.getYear() == yesterday.getYear()) {
-                     formatedDate = "Hier";
-                }
-              }
-              var urlNotif = "https://zestedesavoir.com" + resultsNotification[notif].url;
-              if(_debug) console.log(urlNotif + " by " + senderNotif);
-              addNotification(titleNotif, senderNotif, senderAvatarNotif, formatedDate, urlNotif);
-            }
-          }
-          //Notify the user
-          if(countNotifications > _notifCounter) {
-            if(_debug) console.log("Nouvelles notifications : " + countNotifications);
-            chrome.browserAction.setIcon({path:"icons/icone_n_20.png"});
-            var title = "Zds-notificateur : Nouvelle notification !";
-            var content = "Vous avez " + countNotifications + " notification";
-            if (countNotifications > 1) content += "s";
-            notifyMe(title, content);
-          } else if (countNotifications === 0) {
-            chrome.browserAction.setIcon({path:"icons/clem_48.png"});
-          }
-          _notifCounter = countNotifications;
+          parseNotifications(rootDOM.results)
         }
-      } else {
-        if(_debug) console.log(result);
+      } else if (debugMode) {
+        console.log(result)
       }
     }
 
-
-    if(!_notifCounter) {
-      var divNoNotif = document.createElement('div');
-      divNoNotif.id = "noNotif";
-      divNoNotif.innerHTML = "Aucune notification";
-      _contentDiv.appendChild(divNoNotif);
-      if(_debug) console.log("Aucune notification");
-    }
-    var body = document.body;
-    body.appendChild(_contentDiv);
-    //Remove useless nodes
-    while(body.childNodes.length > 2) {
-      body.removeChild(body.childNodes[1]);
-    }
-    _currentDom = body;
-  };
+    buildPopup()
+  }
 
   xhr.onerror = function (e) {
-    console.error(xhr.statusText);
-    _connected = false;
-  };
-  xhr.send(null);
+    console.error(xhr.statusText)
+    connected = false
+  }
+  xhr.send(null)
 }
 
-/*
-* Add a notification to the DOM
-*/
-function addNotification(title, sender, senderAvatar, date, url) {
-  //Design popup
-  var a = document.createElement('a');
-  a.href = url;
-  a.target = "_blank";
-  var divNotif = document.createElement('div');
-  divNotif.id = "notification";
-  imgAvatar = document.createElement('img');
-  imgAvatar.src = senderAvatar;
-  var divBlocNotif = document.createElement('div');
-  divBlocNotif.id="blocNotif";
-  var divDate = document.createElement('div');
-  divDate.id = "date";
-  divDate.innerHTML = escapeHTML(date);
-  var divPseudo = document.createElement('div');
-  divPseudo.id = "pseudo";
-  divPseudo.innerHTML = escapeHTML(sender);
-  var divTitle = document.createElement('div');
-  divTitle.id = "title";
-  divTitle.innerHTML = escapeHTML(title);
-
-  divBlocNotif.appendChild(divDate);
-  divBlocNotif.appendChild(divPseudo);
-  divBlocNotif.appendChild(divTitle);
-  divNotif.appendChild(imgAvatar);
-  divNotif.appendChild(divBlocNotif);
-  a.appendChild(divNotif);
-  _contentDiv.appendChild(a);
+/**
+ * Build the DOM of the popup
+ */
+function buildPopup () {
+  if (!notifCounter) {
+    const divNoNotif = document.createElement('div')
+    divNoNotif.id = 'noNotif'
+    divNoNotif.innerText = 'Aucune notification'
+    contentDiv.appendChild(divNoNotif)
+    if (debugMode) console.log(divNoNotif.innerText)
+  }
+  const body = document.body
+  body.appendChild(contentDiv)
+  // Remove useless nodes
+  while (body.childNodes.length > 2) {
+    body.removeChild(body.childNodes[1])
+  }
+  currentDom = body
 }
 
-/*
-* Create a notification
-*/
-function notifyMe(title, content) {
-  if(_showNotification) {
+/**
+ * Parse the JSON returned by the API
+ * @param  results the JSON object
+ */
+function parseNotifications (results) {
+  // Get new notifications
+  let notificationsCounter = 0
+  for (const notif of results) {
+    // If a notification is new we have is_read === False
+    if (!notif.is_read) {
+      notificationsCounter += 1
+      const title = notif.title
+      const author = notif.sender.username
+      const authorAvatar = notif.sender.avatar_url
+
+      const urlNotif = `${baseUrl}${notif.url}`
+      if (debugMode) console.log(`${urlNotif} by ${author}`)
+      addNotification(title, author, authorAvatar, formatDate(notif.pubdate), urlNotif)
+    }
+  }
+  // Notify the user
+  if (notificationsCounter > notifCounter) {
+    if (debugMode) console.log(`New notification: ${notificationsCounter}`)
+    chrome.browserAction.setIcon({path: 'icons/icone_n_20.png'})
+    const title = 'Zds-notificateur : Nouvelle notification !'
+    let content = `Vous avez ${notificationsCounter} notification`
+    if (notificationsCounter > 1) content += 's'
+    popupNotification(title, content)
+  } else if (notificationsCounter === 0) {
+    chrome.browserAction.setIcon({path: 'icons/clem_48.png'})
+  }
+  notifCounter = notificationsCounter
+}
+
+/**
+ * Pretty print a publication date
+ * @param  pubdate
+ */
+function formatDate (pubdate) {
+  const date = new Date((pubdate || '').replace(/-/g, '/').replace(/[TZ]/g, ' '))
+
+  const actualDate = new Date()
+  if (date.toDateString() === actualDate.toDateString()) {
+    return 'Aujourd\'hui'
+  }
+
+  const yesterday = actualDate
+  yesterday.setDate(actualDate.getDate() - 1)
+  if (date.toDateString() === yesterday.toDateString()) {
+    return 'Hier'
+  }
+
+  let minutes = `${date.getMinutes()}`
+  if (minutes.length < 2) {
+    minutes = `0${minutes}`
+  }
+  const shortDate = [date.getDate(), date.getMonth() + 1].join('/')
+  const time = [date.getHours(), minutes].join('h')
+  return `le ${shortDate} à ${time}`
+}
+
+/**
+ * Add a notification entry
+ * @param title of the subject
+ * @param author
+ * @param authorAvatar
+ * @param date
+ * @param url
+ */
+function addNotification (title, author, authorAvatar, date, url) {
+  const divBlocNotif = document.createElement('div')
+  divBlocNotif.id = 'blocNotif'
+  const divDate = document.createElement('div')
+  divDate.id = 'date'
+  divDate.innerText = date
+  const divPseudo = document.createElement('div')
+  divPseudo.id = 'pseudo'
+  divPseudo.innerText = author
+  const divTitle = document.createElement('div')
+  divTitle.id = 'title'
+  divTitle.innerText = title
+  const imgAvatar = document.createElement('img')
+  imgAvatar.src = authorAvatar
+  const divNotif = document.createElement('div')
+  divNotif.id = 'notification'
+  const a = document.createElement('a')
+  a.href = url
+  a.target = '_blank' // Open the notification in a new window
+
+  divBlocNotif.appendChild(divDate)
+  divBlocNotif.appendChild(divPseudo)
+  divBlocNotif.appendChild(divTitle)
+  divNotif.appendChild(imgAvatar)
+  divNotif.appendChild(divBlocNotif)
+  a.appendChild(divNotif)
+  contentDiv.appendChild(a)
+}
+
+/**
+ * Show a popup notification
+ * @param  title of the popup
+ * @param  content
+ */
+function popupNotification (title, content) {
+  if (showPopupNotification) {
     chrome.notifications.create({
-      "type": "basic",
-      "iconUrl": chrome.extension.getURL("icons/icone_n_20.png"),
-      "title": title,
-      "message": content
-    });
+      'type': 'basic',
+      'iconUrl': chrome.extension.getURL('icons/icone_n_20.png'),
+      'title': title,
+      'message': content
+    })
   }
 }
 
-//Update the popup
-setInterval(getNotificationsFromAPI, _delayUpdate);
-getNotificationsFromAPI();
+// Update the popup
+setInterval(getNotificationsFromAPI, updateDelay)
+getNotificationsFromAPI()
--- Firefox/popup/notifier.js
+++ Firefox/popup/notifier.js
@@ -1,29 +1,31 @@
-var _notifications = document.getElementById('notificationList');
+/* global chrome close:true */
+/* eslint no-undef: "error" */
+const notificationsList = document.getElementById('notificationList')
 
-function updateUI() {
-  if(chrome.extension.getBackgroundPage()._connected) {
-    var notConnectedDiv = document.getElementById('notConnected');
-    notConnectedDiv.style.display = 'none';
-    var bgNodes = chrome.extension.getBackgroundPage()._currentDom.lastChild.cloneNode(true);
-    _notifications.appendChild(bgNodes);
+function updateUI () {
+  if (chrome.extension.getBackgroundPage().connected) {
+    const notConnectedDiv = document.getElementById('notConnected')
+    notConnectedDiv.style.display = 'none'
+    const bgNodes = chrome.extension.getBackgroundPage().currentDom.lastChild.cloneNode(true)
+    notificationsList.appendChild(bgNodes)
   } else {
-    var connectedDiv = document.getElementById('connected');
-    connectedDiv.style.display = 'none';
+    const connectedDiv = document.getElementById('connected')
+    connectedDiv.style.display = 'none'
   }
 }
 
 // sleep time expects milliseconds
 function sleep (time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
+  return new Promise((resolve) => setTimeout(resolve, time))
 }
 
-_notifications.addEventListener("click", function(){
+notificationsList.addEventListener('click', function () {
   sleep(2000).then(() => {
-    if(chrome.extension.getBackgroundPage()._notifCounter !== 0) {
-      close();
+    if (chrome.extension.getBackgroundPage().notifCounter !== 0) {
+      close()
     }
-    chrome.extension.getBackgroundPage().getNotificationsFromAPI();
-  });
-});
+    chrome.extension.getBackgroundPage().getNotificationsFromAPI()
+  })
+})
 
-updateUI();
+updateUI()
