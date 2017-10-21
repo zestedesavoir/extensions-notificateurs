7531352fc03ec695ee705cc2a3c4f5d306bb5bec
Sébastien Blin <AmarOk1412@users.noreply.github.com>
2017-10-21 17:49:37 -0400

    Firefox ajout d'un thème clair (#51)
    * firefox: Nettoie le code et ajoute un bouton pour créer un nouveau MP
    
    * firefox: ajout d'un thème clair

--- Firefox/options/options.js
+++ Firefox/options/options.js
@@ -1,12 +1,16 @@
 function saveOptions(e) {
   chrome.storage.local.set({
-    notify: document.querySelector("#notify").checked
+    notify: document.querySelector("#notify").checked,
+    white_theme: document.querySelector("#white_theme").checked
   });
 }
 
 function restoreOptions() {
   chrome.storage.local.get('notify', (res) => {
     document.querySelector("#notify").checked = res.notify || false;
+  });
+  chrome.storage.local.get('white_theme', (res) => {
+    document.querySelector("#white_theme").checked = res.white_theme || false;
   });
 }
 
